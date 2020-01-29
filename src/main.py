a3e9152075a680a4626a6466cd157071150868f4
Ning <wanninglim@gmail.com>
2020-01-30 01:10:10 +0800

    Add test case for display menu

---
+++ src/main.py
@@ -0,0 +1,12 @@
+# Display main menu (and allow for repetition)
+def displayMenu():
+    menu = ("MAIN MENU\n" 
+            "=========\n"
+            "[1] Read and load maze from file\n"
+            "[2] View maze\n"
+            "[3] Play maze game\n"
+            "[4] Configure current maze\n\n"
+            "[0] Exit Maze\n")
+
+    print(menu)
+    return menu
