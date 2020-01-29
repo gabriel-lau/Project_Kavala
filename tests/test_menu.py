c52ef7a76de229ad752ad8ae445f563c60fa6b3d
Ning <wanninglim@gmail.com>
2020-01-29 23:12:27 +0800

    Add test for display menu

---
+++ tests/test_menu.py
@@ -0,0 +1,13 @@
+import pytest
+from main import *
+
+# Test for displaying menu
+def test_displayMenu():
+    menu = displayMenu()
+    assert menu == ("MAIN MENU\n" 
+                    "=========\n"
+                    "[1] Read and load maze from file\n"
+                    "[2] View maze\n"
+                    "[3] Play maze game\n"
+                    "[4] Configure current maze\n\n"
+                    "[0] Exit Maze\n")
