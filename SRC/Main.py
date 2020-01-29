833d73cf1c7b85ea295f4fbadf7f6b4fdff7f615
Ning <wanninglim@gmail.com>
2020-01-30 02:24:39 +0800

    Add partial tests for options

--- SRC/Main.py
+++ SRC/Main.py
@@ -1,7 +1,5 @@
-# Display main menu (and allow for repetition)
-# while True:
-
-def displayMenu():
+# Display main menu
+def display_menu():
     menu = ("MAIN MENU\n" 
             "=========\n"
             "[1] Read and load maze from file\n"
@@ -12,3 +10,50 @@ def displayMenu():
 
     print(menu)
     return menu
+
+# Allow user to choose option
+def choose_option():
+    menu_option = int(input("Enter your option: "))
+    return menu_option
+
+# TODO: Uncomment functions when you unit test them. In test_menu, include expected return values
+def menu_function(menu_option):
+    if menu_option == 1:
+        # return readFile()
+        return "Invalid option. Please try again!"
+    elif menu_option == 2:
+        # return displayMaze()
+        return "Invalid option. Please try again!"
+    elif menu_option == 3:
+        # return playGame()
+        return "Invalid option. Please try again!"
+    elif menu_option == 4:
+        # return configureMaze()
+        return "Invalid option. Please try again!"
+    elif menu_option == 0:
+        return "Thanks for playing Maze!"
+    else:
+        return "Invalid option. Please try again!"
+
+# # Reads maze file in .csv format
+# def readFile():
+#     return True
+
+# # Displays the maze
+# def displayMaze():
+#     return True
+
+# # Play maze game
+# def playGame():
+#     return True
+
+# # Configure maze
+# def configureMaze():
+#     return True
+
+# Global -------------------------------------
+# while True:
+#     displayMenu()
+#     if chooseOption() == 0:
+#          break
+#     print()
--- test_menu.py
+++ test_menu.py
@@ -1,9 +1,9 @@
 import pytest
-from SRC.Main import *
+from src.main import *
 
 # Test for displaying menu
 def test_displayMenu():
-    menu = displayMenu()
+    menu = display_menu()
     assert menu == ("MAIN MENU\n" 
                     "=========\n"
                     "[1] Read and load maze from file\n"
@@ -11,3 +11,19 @@ def test_displayMenu():
                     "[3] Play maze game\n"
                     "[4] Configure current maze\n\n"
                     "[0] Exit Maze\n")
+
+# TODO: Make unit tests for readFile(), displayMaze() , playGame() & configureMaze() once these functions are done
+@pytest.mark.parametrize("option",[(1), (2), (3), (4), (5), (0), (-2), (-10), ('letters'), ('$$^@!')])
+def test_SelectOption(option):
+    # if option == 1:
+    #     assert menu_function(1) == <return function for 1>
+    # elif option == 2:
+    #     assert menu_function(2) == <return function for 2>
+    # elif option == 3:
+    #     assert menu_function(3) == <return function for 3>
+    # elif option == 4:
+    #     assert menu_function(4) == <return function for 4>
+    if option == 0:
+        assert menu_function(0) == "Thanks for playing Maze!"
+    else:
+        assert menu_function(option) == "Invalid option. Please try again!"
