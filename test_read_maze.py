33df1e40cdf023bd89dfebec90f83d61339d6abe
Ning <wanninglim@gmail.com>
2020-02-05 23:32:00 +0800

    Fix #28: Upload non-existent .csv file

--- test_read_maze.py
+++ test_read_maze.py
@@ -8,23 +8,33 @@ maze_no_start_end = [['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'], ['X', 'O', 'O', '
 # ==================================================================
 # Test for reading files
 
+# Adding a normal maze returns a list with the maze coordinates
 def test_read_maze_normal():
     with mock.patch('builtins.input', return_value='maze.csv'):
         assert read_file([]) == maze_normal
-    
-def test_read_maze_empty():
-    with mock.patch('builtins.input', return_value='maze-empty.csv'):
-        assert read_file([]) == []
 
+# Adding a maze with no start and end point returns a list with the maze coordinates
 def test_read_maze_no_start_end():
     with mock.patch('builtins.input', return_value='maze-no-start-end.csv'):
         assert read_file([]) == maze_no_start_end
 
+# Adding an empty maze file returns empty maze_list
+def test_read_maze_empty():
+    with mock.patch('builtins.input', return_value='maze-empty.csv'):
+        assert read_file([]) == []
+
+# Adding a csv file that does not exist returns empty maze_list
+def test_read_csv_no_exist():
+    with mock.patch('builtins.input', return_value='fake.csv'):
+        assert read_file([]) == []
+
+# Check if function store_start_end knows if maze has no start (A) or end (B) point
 def test_store_start_end_name_error():
     with mock.patch('sys.stdout', new=io.StringIO()) as fake_stdout: 
         store_start_end([])
     assert fake_stdout.getvalue() == 'Maze does not have a start or end point.\n'
 
+# Check if function store_start_end records the value of the start (A) and end (B) point
 def test_store_start_end_row_exist():
     value = store_start_end(maze_normal)
     assert value == [[2, 7], [8, 2]]
